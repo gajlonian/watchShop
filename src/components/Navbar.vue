ff5cde211d0e563e052b7640acbe7461c0af0b04
Franckie <voninjatovofranckie@gmail.com>
2023-03-14 14:21:29 +0000

    version 1.0.1

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,14 +4,7 @@ import { mapState } from 'vuex';
     <nav class="navbar navbar-expand-md navbar-light container">
         <div class="container">
             <router-link class="navbar-brand" to="/">ShopE</router-link>
-            <button
-                class="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarCollapse"
-                aria-controls="navbarCollapse"
-                aria-expanded="false"
-                aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarCollapse">
@@ -31,20 +24,23 @@ import { mapState } from 'vuex';
                 </ul>
                 <div class="lists">
                     <a href="#" @click.prevent="showShoppingCart">
-                        <i href="#" class="fa fa-shopping-cart" style="font-size:25px; color:rgba(0,0,0, .5)" onmouseover="this.style.color='#000000'" onmouseout="this.style.color='rgba(0,0,0, .5)'"></i>
+                        <i href="#" class="fa fa-shopping-cart" style="font-size: 25px; color: rgba(0, 0, 0, 0.5)" onmouseover="this.style.color='#000000'" onmouseout="this.style.color='rgba(0,0,0, .5)'"></i>
                     </a>
                     <div class="carts mt-1" :class="{ active: isActive }">
-                            <ul class="list-group px-2" v-for="product in cart" :key="product.id">
-                                <li class="list-group-item m-0 d-flex justify-content-between">
-                                    <span class="d-block"><img :src="product.thumbnail" alt=""></span>
-                                    <div class="my-auto">
-                                        <h6 class="d-inline mx-2">: {{ product.price }}</h6>
-                                        <span @click="removeProduct(product)">
-                                            <i class="fas fa-trash"></i>
-                                        </span>
-                                    </div>
-                                </li> <hr>
-                            </ul>
+                        <div class="cartSms" v-if="this.$store.state.cart == 0">Empty</div>
+
+                        <ul class="list-group px-2" v-for="product in cart" :key="product.id">
+                            <li class="list-group-item m-0 d-flex justify-content-between">
+                                <span class="d-block"><img :src="product.thumbnail" alt="" /></span>
+                                <div class="my-auto">
+                                    <h6 class="d-inline mx-2">: {{ product.price }}</h6>
+                                    <span @click="removeProduct(product)">
+                                        <i class="fas fa-trash"></i>
+                                    </span>
+                                </div>
+                            </li>
+                            <hr />
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -53,67 +49,78 @@ import { mapState } from 'vuex';
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions } from "vuex";
 
 export default {
     name: "Navbar",
-    data () {
+    data() {
         return {
-            isActive: false
-        }
+            isActive: false,
+        };
     },
 
     methods: {
-        showShoppingCart () {
-            if(!this.isActive) {
-                return this.isActive = true
+        showShoppingCart() {
+            if (!this.isActive) {
+                return (this.isActive = true);
             } else {
-                return this.isActive = false
+                return (this.isActive = false);
             }
         },
-        ...mapActions(['removeProduct'])
+        ...mapActions(["removeProduct"]),
     },
-    computed : {
+    computed: {
         cart() {
-            return this.$store.state.cart
-        }
-    }
+            return this.$store.state.cart;
+        },
+    },
 };
 </script>
 
 <style scoped>
-    a {
-        text-decoration: none;
-        outline: none;
-    }
-    .navbar-brand {
-        color: rgba(0, 0, 0, .5);
-        font-weight: 500;
-    }
-    .carts {
-        width: 200px;
-        height: 0;
-        background-color: rgb(55, 136, 138, .8);
-        position: absolute;
-        right: 13px;
-        border-radius: 5px;
-        z-index: 1000;
-        overflow: hidden;
-        transition: height 500ms ease;
-        padding: 0;
-    }
-    .active {
-        height: 400px;
-    }
+a {
+    text-decoration: none;
+    outline: none;
+}
+.navbar-brand {
+    color: rgba(0, 0, 0, 0.5);
+    font-weight: 500;
+}
+.carts {
+    width: 200px;
+    height: 0;
+    background-color: rgb(55, 136, 138, 0.9);
+    position: absolute;
+    right: 13px;
+    border-radius: 5px;
+    z-index: 1000;
+    overflow: hidden;
+    transition: height 500ms ease;
+    padding: 0;
+}
+.active {
+    height: 400px;
+}
 
-    img {
-        width: 55px;
-        height: 55px;
-        border-radius: 50px;
-    }
+img {
+    width: 55px;
+    height: 55px;
+    border-radius: 50px;
+}
 
-    .list-group-item {
-        background-color: inherit;
-        border: none;
-    }
+.list-group-item {
+    background-color: inherit;
+    border: none;
+}
+
+.cartSms {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: 500;
+    font-size: 1.5rem;
+    color: rgb(0, 0, 0, 0.8);
+}
 </style>
--- src/views/Watchs.vue
+++ src/views/Watchs.vue
@@ -1,52 +1,54 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-4" v-for="product in this.getData" :key="product.id">
-        <div class="card">
-          <div class="card-body">
-            <img :src="product.url" alt="smartwatch" class="card-img"> <hr>
-            <h6 class="card-title">{{ product.title }}</h6>
-            <p class="card-text">{{ product.description }}</p>
-            <p class="card-text">Prices: <span>50 $</span></p>
-            <button class="btn btn-outline-success" @click="addToCart(product)">ADD TO CART</button>
-          </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-4" v-for="product in this.getData" :key="product.id">
+                <div class="card">
+                    <div class="card-body">
+                        <img :src="product.url" alt="smartwatch" class="card-img" />
+                        <hr />
+                        <h6 class="card-title">{{ product.title }}</h6>
+                        <p class="card-text">{{ product.description }}</p>
+                        <p class="card-text">
+                            Prices: <span>{{ product.price }}</span>
+                        </p>
+                        <button class="btn btn-outline-success" @click="addToCart(product)">ADD TO CART</button>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
-<script >
-  import { mapGetters} from 'vuex';
-    export default { 
-      computed: {
+<script>
+import { mapGetters } from "vuex";
+export default {
+    computed: {
         ...mapGetters(["getData"]),
-      },
+    },
 
-      mounted() {
-        this.$store.dispatch('fetchData')
-          .then(() => {
-           return this.getData
-          })
-      },
-      
-      methods : {
+    mounted() {
+        this.$store.dispatch("fetchData").then(() => {
+            return this.getData;
+        });
+    },
+
+    methods: {
         addToCart(product) {
-          this.$store.dispatch('addToCart', product);
-        }
-      }
-    }
+            this.$store.dispatch("addToCart", product);
+        },
+    },
+};
 </script>
 
 <style scoped>
-   .card {
+.card {
     background-color: #d3d3d3;
     width: 100%;
     border: none;
     transition: transform 250ms ease, background-color 100ms ease-in-out;
-   }
-   .card:hover {
-      transform: scale(1.1);
-      background-color: rgb(55, 136, 138, .1);
-   }
+}
+.card:hover {
+    transform: scale(1.1);
+    background-color: rgb(55, 136, 138, 0.1);
+}
 </style>
